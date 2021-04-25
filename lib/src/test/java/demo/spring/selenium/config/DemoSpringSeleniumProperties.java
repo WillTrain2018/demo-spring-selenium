9e45d487465fe4297d03e5917131d1a8c3999855
Joe Williams <Joe.Williams@nike.com>
2021-04-25 14:22:06 -0700

    update project

---
+++ lib/src/test/java/demo/spring/selenium/config/DemoSpringSeleniumProperties.java
@@ -0,0 +1,23 @@
+package demo.spring.selenium.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties
+@EnableConfigurationProperties
+public class DemoSpringSeleniumProperties {
+
+    @Value("${host}")
+    private String host;
+
+    @Value("${browser}")
+    private String browser;
+
+    @Value("${context}")
+    private String context;
+}
---
+++ lib/src/test/java/demo/spring/selenium/stepdefinitions/SecureSteps.java
@@ -0,0 +1,18 @@
+package demo.spring.selenium.stepdefinitions;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import demo.spring.selenium.pages.SecurePage;
+import io.cucumber.java.en.Then;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SecureSteps {
+
+    @Autowired private SecurePage securePage;
+
+    @Then("^I see \"(.*)\" message$")
+    public void iSeeMessage(String message) {
+        assertThat(this.securePage.getMessage(), is(message));
+    }
+}
